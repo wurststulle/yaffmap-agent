1b166f193840b76217ff0328e6dd5820a50df1f4
glockman <glockman@5eed78a2-3918-4851-bc8f-e53b4248f697>
2010-09-05 16:28:02 +0000

    hopefully the last adoption to big database changes
    git-svn-id: http://192.168.2.3/yaffmap/svn/trunk/update_agents/luci@83 5eed78a2-3918-4851-bc8f-e53b4248f697

--- ffmapupdate_uci.sh
+++ ffmapupdate_uci.sh
@@ -53,13 +53,6 @@ done
 #
 #######
 
-ip_params(){
-	network=$1
-	config_get_batch "$network" ipaddr ip6addr
-		
-	[ -n "$ipaddr" ] && attr ipv4Addr $ipaddr
-	[ -n "$ip6addr" ] && attr ipv6Addr $ip6Addr
-}
 
 config_get_batch(){
 	for s in $*
@@ -98,9 +91,39 @@ EOF
 
 }
 
+interface_addresses(){
+
+	ip_params(){		
+		local config=$1
+		config_get_batch "$config" ipaddr ip6addr
+		[ -n "$ipaddr" ] && attr ipv4Addr $ipaddr
+		[ -n "$ip6addr" ] && attr ipv6Addr $ip6Addr
+	}
+
+	local config=$1
+	config_get_batch "$config" ifname type aliases
+	macaddr=$( ip addr show dev $ifname | grep -e '.*:.*:.*:.*:.*:.*' | cut -d" " -f 6 )
+	attr macAddr $macaddr
+	ip_params $config
+	[ "$type" = "bridge" ] && attr bridgeName "$ifname"
+
+	[ -n "$aliases" ] && {
+		array ipAlias
+			for a in $aliases
+			do
+				obj
+					config_get ifname "$a" ifname
+					attr name $ifname
+					ip_params $a
+				endobj
+			done
+		endarr
+	}
+}
+
 
 wifi_device_attributes(){
-#	echo "entered wifi_device_attributes()"
+	echo "entered wifi_device_attributes()"
 	
 	wifi_iface_attributes(){
 #		echo "entered wifi_iface_attributes()"
@@ -109,18 +132,19 @@ wifi_device_attributes(){
 		config_get device "$config" device
 		if [ "$device" = "$2" ]
 		then
-			config_get_batch "$config" ssid bssid mode
+			config_get_batch "$config" ssid bssid mode ifname
 			
 			obj
 				[ -n "$ssid" ] && attr essid $ssid
 				[ -n "$bssid" ] && attr bssid $bssid
 				[ -n "$mode" ] && attr wlMode $mode
-					config_get network "$config" network
-				eval "ifname=\$CONFIG_${network}_ifname"
+				config_get network "$config" network
+				config_get ifname "$network" ifname
 				attr name $ifname
-				ip_params $network
+				wlmac=$( ip addr show dev $ifname | grep -e '.*:.*:.*:.*:.*:.*' | cut -d" " -f 6 )
+				attr wlMacAddr $wlmac
+				interface_addresses $network -w
 			endobj
-			net_aliases $network 			
 		fi
 	} 
 
@@ -130,55 +154,33 @@ wifi_device_attributes(){
 	
 	config_load freifunk_map
 	config_get ignore "$config" ignore
-	config_load wireless
 	
 	if [ "$ignore" != "1" ]
 	then
-		config_get_batch "$config" channel hwmode txpower hwmode macaddr antDirection antGain antBeamH antBeamV antPol antTilt
+		config_load wireless
+
+		config_get_batch "$config" type channel hwmode txpower macaddr antDirection antGain antBeamH antBeamV antPol antTilt macaddr
 		obj
-			attr name $config
+			[ "$type" = "atheros" ] && attr name $config
 			
 			for s in antDirection antGain antBeamH antBeamV antPol antTilt channel txpower
 			do
 				eval "var_cont=\$$s"
 				[ -n "$var_cont" ] && attr $s $var_cont
 			done
+			[ -n "$hwmode" ] && attr wirelessStandard "802.$hwmode"
 			
-			obj deviceType
-				attr isWireless "1"
-				[ -n "$hwmode" ] && attr wirelessStandard "802.$hwmode"
-			endobj
-			
-			if [ -z "$macaddr" ]
-			then
-				macaddr=$( ip addr show dev $config | grep -e '.*:.*:.*:.*:.*:.*' | cut -d" " -f 6 )
-			fi
-			attr macAddr $macaddr
-			
-			array iface
+			array wlIface
 				config_foreach wifi_iface_attributes wifi-iface $config
 			endarr
 		endobj
 	fi
 } 
 
-net_aliases(){
-	config_get aliases "$1" aliases
-	for a in $aliases
-	do
-		obj
-			config_get ifname "$a" ifname
-			attr name $ifname
-			ip_params $a
-		endobj
-	done
-}
-
 network_interfaces(){
 #  echo "entered network_interfaces()"
 	network_iface(){
 		local config=$1
-		local iswireless=0
 		local isbridge=0
 		
 		[ "$VIRGIN_MODE" = "1" ] && wired_default_config	$config
@@ -189,36 +191,34 @@ network_interfaces(){
 
 		if [ "$ignore" != "1" ]
 		then
-			config_get type "$config" type
-			[ "$type" = "bridge" ] && isbridge=1
-		
 			obj
-				obj deviceType
-					attr isWireless 0
-					attr bandwidth 100M
-					attr duplex full
-				endobj
 				config_get ifname "$config" ifname
-				macaddr=$( ip addr show dev $ifname | grep -e '.*:.*:.*:.*:.*:.*' | cut -d" " -f 6 )
-				attr macAddr $macaddr
-				array iface
-					obj
-						attr name $ifname
-						ip_params $config
-					endobj
-					net_aliases $config
-				endarr
+				attr name $ifname
+				interface_addresses $config
 			endobj
 		fi
 	}
 
-	array device
+	array wiredIface
 		config_load network
 		scan_interfaces
 		config_foreach network_iface interface
+	endarr
 
+	array wlDevice
+		config_cb(){
+			case "$1" in
+				"wifi-device" ) WIFI_DEVICES="$WIFI_DEVICES $2"
+				;;
+			esac
+		}
 		config_load wireless
-		config_foreach wifi_device_attributes wifi-device
+		reset_cb
+		for wd in $WIFI_DEVICES
+		do
+			wifi_device_attributes $wd
+		done
+#		config_foreach wifi_device_attributes wifi-device
 	endarr
 }
 
@@ -306,27 +306,26 @@ config 'ffmap' 'ffmap'
 
 EOF
 	VIRGIN_MODE=1
+
+	macAddr=$( ip addr show dev eth0 | grep -e '.*:.*:.*:.*:.*:.*' | cut -d" " -f 6 )
+	returnstring=$( wget -T30 -q -q -O- "$URL?do=getID&type=wired&macAddr=$macAddr" )
+	errorcode=$( echo $returnstring | cut -d"|" -f1 )
+	errormessage=$( echo $returnstring | cut -d"|" -f2 )
+	returndata=$( echo $returnstring | cut -d"|" -f3 )
+	
+	eval "$returndata"
+
+	uci set freifunk_map.ffmap.id="$id"
+	uci commit
+	
 fi
 
 config_load freifunk_map
 config_get nodeid ffmap id
-
-if [ "$nodeid" = "0" -o -z "$nodeid" ]
+if [ "$nodeid" = "0" -o -z "$nodeid" ] 
 then
-	echo moep
-	macAddr=$( ip addr show dev eth0 | grep -e '.*:.*:.*:.*:.*:.*' | cut -d" " -f 6 )
-	echo bla
-	returnstring=$( wget -T30 -q -q -O- "$URL?do=getID&macAddr=$macAddr" )
-	echo keks
-	errorcode=$( echo $returnstring | cut -d"|" -f1 )
-	errormessage=$( echo $returnstring | cut -d"|" -f2 )
-	returndata=$( echo $returnstring | cut -d"|" -f3 )
-	eval "$returndata"
-
-	echo "$returnstring"
-	config_set id ffmap $id
-
-	uci commit
+	echo "Error while getting ID. NodeID is 0 or empty."
+	return 0
 fi
 
 full_node_update
@@ -335,6 +334,7 @@ URL="$URL?do=update&"$DATA
 
 #length=${#URL}
 #echo "\$URL is $length long"
-#echo $URL
+echo $URL
 
-wget -T30 -q -O- $URL
+returnstring=$( wget -T30 -q -O- $URL )
+echo $returnstring
