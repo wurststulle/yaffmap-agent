8b20a471c47ceb0c7631c8ff7ae4701497921836
glockman <glockman@5eed78a2-3918-4851-bc8f-e53b4248f697>
2010-09-08 03:34:42 +0000

    a draft script for global olsr map update
    git-svn-id: http://192.168.2.3/yaffmap/svn/trunk/update_agents/luci@111 5eed78a2-3918-4851-bc8f-e53b4248f697

---
+++ globalupdate_uci.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+. /etc/functions.sh
+
+TEMP_DIR="."
+
+get_dnssuffix(){
+	config_get suffix "$1" suffix
+}
+
+config_load olsrd
+config_foreach get_dnssuffix LoadPlugin
+
+echo $suffix
+
+cp /var/etc/hosts.olsr $TEMP_DIR
+cp /var/run/latlon.js $TEMP_DIR
+
+cat <<EOF > $TEMP_DIR/latlon_crawler.awk
+#!/usr/bin/awk -f
+EOF
+
+chmod 755 latlon_crawler.awk
+
+#exec << EOM
+# $( grep -v -e  mid[1-9]\. ./hosts.olsr  | grep ^[1-9] | grep -v 127\.0\.0\.1 ) 
+#EOM
+#
+#n=0
+#
+#while read mainip hostname comment
+#do 
+#	n = `expr $n + 1 `
+#	echo "hosts_name[$n,\"name\"]=\"$hostname\"; hosts_name[$n,\"ip\"]=\"$mainip\""
+#done 
+
+awk '
+BEGIN{
+	print "BEGIN{"
+}
+{
+if ($2!~/^mid[1-9]\./)
+	if ($1~/^[1-9]/) 
+		if ($1!~/127\.0\.0\./) {
+			print "hosts_names[\""$2","$1"\"]" 
+		}
+}
+END{
+	print "}"
+}' $TEMP_DIR/hosts.olsr >> $TEMP_DIR/latlon_crawler.awk
+
+
+
+cat <<EOM >> $TEMP_DIR/latlon_crawler.awk
+{
+gsub(");","")
+gsub(","," ")
+gsub("'","")
+
+if (\$1~mainip) {
+
+	if ( \$1~/^Mid/ ) {
+		gsub("Mid(","")
+		mid[\$1]=mid[$1]"obj;attr ipv4Addr "\$2";endobj;"
+	}
+	if  (\$1~/^Node/ ) {
+		captureresult=""
+		gsub("Node(","")
+		print "obj node;attr name "\$6";attr latitude "\$2";attr longitude "\$3";array iface;"
+		print "obj;attr ipv4addr "\$1";endobj;"mid[\$1]"endarr;endobj;"
+		hostplussuffix=\$6"$suffix"
+		for ( s in hosts_names ) {
+			split( s, separate, "," )
+			if ( separate[1] == hostplussuffix )
+				delete hosts_names[s]
+		}
+	}
+}
+}
+END{
+for ( s in hosts_names ) {
+	split( s, separate, "," )
+	print "obj node;attr name "separate[1]";array iface;obj;attr ipv4addr "separate[2]":endobj;endarr"
+}
+}
+EOM
+
+$TEMP_DIR/latlon_crawler.awk $TEMP_DIR/latlon.js
+rm $TEMP_DIR/latlon_crawler.awk $TEMP_DIR/latlon.js $TEMP_DIR/hosts.olsr
