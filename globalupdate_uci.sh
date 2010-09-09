dc8a1062159622916d69c52812d4b3f1e1c27cb0
glockman <glockman@5eed78a2-3918-4851-bc8f-e53b4248f697>
2010-09-09 10:13:04 +0000

    git-svn-id: http://192.168.2.3/yaffmap/svn/trunk/update_agents/luci@124 5eed78a2-3918-4851-bc8f-e53b4248f697

--- globalupdate_uci.sh
+++ globalupdate_uci.sh
@@ -15,35 +15,42 @@ cp /var/run/latlon.js $TEMP_DIR
 
 cat <<EOF > $TEMP_DIR/latlon_crawler.awk
 #!/usr/bin/awk -f
+BEGIN{
 EOF
 
 chmod 755 latlon_crawler.awk
 
+#wget -O- -q http://127.0.0.1:2006/mid | awk '{
+#	if ($1~/^[1-9]/){
+#		gsub(";"," ")
+#		for (i = 2; i <= NF; i++)
+#			temp=temp"obj;attr ipv4Addr "$i";endobj;"
+#			
+#		print "	mid["$1"]=\""temp"\""
+#		temp=""
+#	}
+#}
+#' >> $TEMP_DIR/latlon_crawler.awk
+
 awk '
-BEGIN{
-	print "BEGIN{"
-	print "print \"array node;\""
-}
 {
 if ($2!~/^mid[1-9]\./)
 	if ($1~/^[1-9]/) 
 		if ($1!~/127\.0\.0\./) {
-			print "hosts_names[\""$2","$1"\"]" 
+			print "	hosts_names[\""$2","$1"\"]" 
 		}
 }
-END{
-	print "}"
-}' $TEMP_DIR/hosts.olsr >> $TEMP_DIR/latlon_crawler.awk
+' $TEMP_DIR/hosts.olsr >> $TEMP_DIR/latlon_crawler.awk
 
 
 
 cat <<EOM >> $TEMP_DIR/latlon_crawler.awk
+	print "array node;"
+}
 {
-gsub(");","")
-gsub(","," ")
-gsub("'","")
-
-if (\$1~mainip) {
+	gsub(");","")
+	gsub(","," ")
+	gsub("'","")
 
 	if ( \$1~/^Mid/ ) {
 		gsub("Mid(","")
@@ -53,23 +60,28 @@ if (\$1~mainip) {
 		gsub("Node(","")
 		print "obj;attr name "\$6";attr latitude "\$2";attr longitude "\$3";array iface;"
 		print "obj;attr ipv4addr "\$1";endobj;"mid[\$1]"endarr;endobj;"
+		delete mid[\$1]
 		hostplussuffix=\$6"$suffix"
 		for ( s in hosts_names ) {
 			split( s, separate, "," )
 			if ( separate[1] == hostplussuffix )
 				delete hosts_names[s]
 		}
-	}
 }
 }
 END{
-for ( s in hosts_names ) {
-	split( s, separate, "," )
-	print "obj;attr name "separate[1]";array iface;obj;attr ipv4addr "separate[2]":endobj;endarr"
-}
-print "endarr"
+	for ( s in hosts_names ) {
+		split( s, separate, "," )
+		print "obj;attr name "separate[1]";array iface;obj;attr ipv4addr "separate[2]";endobj;"mid[separate[2]]"endarr"
+		delete mid[separate[2]]
+	}
+	for ( m in mid ){
+		print "m: "m" mid[m]: "mid[m]
+		print "obj;array iface;attr ipv4addr "m";endobj;"mid[m]"endarr;endobj;"
+		}
+	print "endarr"
 }
 EOM
 
 $TEMP_DIR/latlon_crawler.awk $TEMP_DIR/latlon.js
-rm $TEMP_DIR/latlon_crawler.awk $TEMP_DIR/latlon.js $TEMP_DIR/hosts.olsr
+#rm $TEMP_DIR/latlon_crawler.awk $TEMP_DIR/latlon.js $TEMP_DIR/hosts.olsr
