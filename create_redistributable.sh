e8346bac9dfa8e03d1aa45cce33ba920b47a8170
glockman <glockman@5eed78a2-3918-4851-bc8f-e53b4248f697>
2010-09-11 03:31:59 +0000

    splitted update scripts into multiple files to make it easier to support multiple platforms and routing protocols
    git-svn-id: http://192.168.2.3/yaffmap/svn/trunk/update_agents/luci@138 5eed78a2-3918-4851-bc8f-e53b4248f697

--- create_redistributable.sh
+++ create_redistributable.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-#if [ -z "$1" ]
-#then
-#	echo "you missed to give platform name"
-#fi
+if [ -z "$1" ]
+then
+	echo "you missed to give platform name"
+	return 0
+fi
 
 cd files
-tar --exclude=".svn" --exclude="*~"  -cf ../luci_redist.tar *
+tar --exclude=".svn" --exclude="*~"  -cf ../$1_redist.tar etc/init.d/yaffmap lib/yaffmap/common* lib/yaffmap/$1*
